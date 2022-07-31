dae4ec73f791c926e53b9bb7be2e48ecb615c853
omar <altalhi012@gmail.com>
2022-07-31 18:20:19 +0300

    adding employee and edit employee profiles and delete employee profiles

--- resources/views/employee/delete.blade.php
+++ resources/views/employee/delete.blade.php
@@ -36,32 +36,32 @@
                     <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
                         <div class="px-2">
                             @if(session()->has("success"))
-                            <div class="alert alert-success">{{session()->get('success')}}</div>
+                                <div class="alert alert-danger">{{session()->get('success')}}</div>
                             @endif
-                            <form action="{{route("store.employee")}}" method="post" class="justify-content-center" style="color:#000">
+                            <form action="{{route("delete.employee", $employee->id)}}" method="post" class="justify-content-center" style="color:#000">
                                 @csrf
                                 <div class="form-group">
                                     <label class="sr-only">Name</label>
-                                    <input name="name" type="text" class="form-control">
+                                    <input name="name" type="text" class="form-control" value="{{$employee->name}}">
                                     @error('name')
                                     <small class="text-danger">{{$message}}</small>
                                     @enderror
                                 </div>
                                 <div class="form-group">
                                     <label class="sr-only">Age</label>
-                                    <input name="age" type="text" class="form-control">
+                                    <input name="age" type="text" class="form-control" value="{{$employee->age}}">
                                     @error('age')
                                     <small class="text-danger">{{$message}}</small>
                                     @enderror
                                 </div>
                                 <div class="form-group">
                                     <label class="sr-only">Country</label>
-                                    <input name="country" type="text" class="form-control">
+                                    <input name="country" type="text" class="form-control" value="{{$employee->country}}">
                                     @error('country')
                                     <small class="text-danger">{{$message}}</small>
                                     @enderror
                                 </div>
-                                <button type="submit" class="btn btn-secondary btn-lg" style="margin-top: 20px">Save</button>
+                                <button type="submit" class="btn btn-info btn-lg" style="margin-top: 20px">Delete</button>
                             </form>
                         </div>
                     </div>
--- resources/views/employee/display.blade.php
+++ resources/views/employee/display.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <table class="table table-dark">
+    <table class="table thead-light">
         <thead class="thead-light">
         <tr>
-            <th scope="col">#</th>
+            <th scope="col">ID</th>
             <th scope="col">Name</th>
             <th scope="col">Age</th>
             <th scope="col">Country</th>
@@ -18,17 +18,24 @@
             <td>{{$employee->name}}</td>
             <td>{{$employee->age}}</td>
             <td>{{$employee->Country}}</td>
-            <td><a href="#" class="btn btn-primary">Edit</a>
+            <td><a href="{{route("employee.edit",$employee->id)}}" class="btn btn-primary">Edit</a>
                 &nbsp;
-                <a href="#" class="btn btn-danger">Delete</a></td>
+                <a href="{{route("delete.employee",$employee->id)}}" class="btn btn-danger">Delete</a></td>
+
         </tr>
         @endforeach
+        @if(session()->has("success"))
+            <div class="alert alert-success">{{session()->get('success')}}</div>
+        @endif
+        @if(session()->has("error"))
+            <div class="alert alert-danger">{{session()->get('error')}}</div>
+        @endif
         </tbody>
     </table>
     <div class="container">
         <div class="row">
             <div class="col text-center">
-                <a href="{{route("add.employee")}}" class="btn btn-default">Add User</a>
+                <a href="{{route("add.employee")}}" class="btn btn-secondary">Add User</a>
             </div>
         </div>
     </div>
